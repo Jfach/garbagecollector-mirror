bebc9160d26a914eea71775f720773feed2c808d
Jordan Facibene <jordan.facibene13@stjohns.edu>
2016-01-04 13:07:37 -0500

    Create pep8_test.py

---
+++ tests/pep8_test.py
@@ -0,0 +1,18 @@
+import os
+import pep8
+
+
+class TestPep8(Fixture)
+
+def test():
+    """
+    Run PEP8 tests.
+    """
+    checker = pep8.StyleGuide(paths=[os.curdir], reporter=pep8.StandardReport)
+    report = checker.check_files()
+    result = report.total_errors
+    output = "\n".join(report.get_statistics())
+    if result != 0:
+        raise Exception("\n"+output)
+    else:
+        pass    
