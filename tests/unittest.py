d2069926dc5a9877e5cbf36326c742aa8a2136d3
Jordan Facibene <jordan.facibene13@stjohns.edu>
2016-01-04 13:05:52 -0500

    Update unittest.py

--- tests/unittest.py
+++ tests/unittest.py
@@ -1,15 +1,15 @@
 import os
 import unittest
-import pep8
+
+REPO_DIR = os.path.abspath('..')
 
 
-class TestPep8(unittest.TestCase):
-    def test_conformance(self):
-        paths = [os.curdir]
-        checker = pep8.StyleGuide(paths=paths, reporter=pep8.StandardReport)
-        report = checker.check_files()
-        result = report.total_errors
-        output = "\n".join(report.get_statistics())
-        if result != 0:
-            self.fail("Found PEP8 errors:\n%s" % output)
-    
+class TestFixture(unittest.TestCase):
+    """
+    Fixture for unit tests
+    """
+    def setUp(self):
+        print
+        print
+        print("Testing in: %s" % REPO_DIR)
+        print
