0fed01d0630eefabfa1b6efcc25dc60ec92ef808
Jordan Facibene <jordan.facibene13@stjohns.edu>
2016-01-04 13:11:17 -0500

    Update pep8_test.py

--- tests/pep8_test.py
+++ tests/pep8_test.py
@@ -1,18 +1,20 @@
 import os
 import pep8
+from unittest import TestFixture, REPO_DIR
 
 
-class TestPep8(Fixture)
-
-def test():
-    """
-    Run PEP8 tests.
-    """
-    checker = pep8.StyleGuide(paths=[os.curdir], reporter=pep8.StandardReport)
-    report = checker.check_files()
-    result = report.total_errors
-    output = "\n".join(report.get_statistics())
-    if result != 0:
-        raise Exception("\n"+output)
-    else:
-        pass    
+class TestPep8(TestFixture):
+    def setUp(self):
+        TestFixture.setUp(self)
+        
+    def test_conformance(self):
+        """
+        Run PEP8 tests.
+        """
+        checker = pep8.StyleGuide(paths=[REPO_DIR], reporter=pep8.StandardReport)
+        report = checker.check_files()
+        result = report.total_errors
+        output = "\n".join(report.get_statistics())
+        if result != 0:
+            self.fail("Found PEP8 errors:\n%s" % output)
+   
